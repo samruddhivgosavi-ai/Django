3e58485c5cdb429d03a67bbc1f87f75fc78d34c1
samruddhivgosavi-ai <samruddhi.vgosavi@gmail.com>
2026-04-12 21:41:42 +0530

    admin.py and models.py

--- mysite/myapp/admin.py
+++ mysite/myapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from.models import Students
+from.models import Employee
 # Register your models here.
+admin.site.register([Students])
+admin.site.register([Employee])
--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Students(models.Model):
+    name=models.CharField(max_length=100)
+    email=models.CharField(max_length=100)
+    password=models.CharField(max_length=100)
+    contact=models.CharField(max_length=100)
+    city=models.CharField(max_length=100,default="Pune")
+
+class Employee(models.Model):
+    emp_name=models.CharField(max_length=100)
+    emp_email=models.CharField(max_length=100)
+    emp_code=models.CharField(max_length=100)
+    emp_phone=models.CharField(max_length=100)
+    emp_designation=models.CharField(max_length=100)
+    emp_salary=models.CharField(max_length=100)
+    emp_address=models.CharField(max_length=100)
+    emp_department=models.CharField(max_length=100,default="IT")
