a7606eb350e17780ce38531a24e1c07f03022450
samruddhivgosavi-ai <samruddhi.vgosavi@gmail.com>
2026-04-19 21:56:48 +0530

    models

--- mysite/myapp/admin.py
+++ mysite/myapp/admin.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 from.models import Students
 from.models import Employee
 from.models import document
+from.models import Person
 # Register your models here.
 admin.site.register([Students])
 admin.site.register([Employee])
 admin.site.register([document])
+admin.site.register([Person])
